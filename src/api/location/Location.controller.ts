1cfd46b0d8638d75dbe939552c4285707a993861
Safa Günay <safagunay96@gmail.com>
2024-11-29 23:27:55 +0300

    fix(locations): controller handler naming typo

--- src/api/location/Location.controller.ts
+++ src/api/location/Location.controller.ts
@@ -7,7 +7,7 @@ export class LocationController {
   constructor(private readonly queueService: QueueService) {}
 
   @Post()
-  async sendLocation(@Body() payload: Location[]): Promise<void> {
+  async sendLocations(@Body() payload: Location[]): Promise<void> {
     if (!Array.isArray(payload) || !payload.length || payload.length > 100) {
       throw new BadRequestException(
         'body must be a JSON array of min length 1 and max length 100',
