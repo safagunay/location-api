16a23d353ea2912ee0db5962659268989d54b154
Safa Günay <safagunay96@gmail.com>
2024-11-29 22:44:25 +0300

    refactor: update naming of controller files

--- src/api/area/Area.module.ts
+++ src/api/area/Area.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { CacheModule, LoggerModule, TypeOrmModule } from 'src/infra';
-import { AreaController } from './area.controller';
+import { AreaController } from './Area.controller';
 
 @Module({
   imports: [TypeOrmModule, CacheModule, LoggerModule],
--- src/api/location/Location.module.ts
+++ src/api/location/Location.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { QueueModule } from 'src/infra';
-import { LocationController } from './location.controller';
+import { LocationController } from './Location.controller';
 
 @Module({
   imports: [QueueModule],
