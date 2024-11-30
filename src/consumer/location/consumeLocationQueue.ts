a47d38804bfd25de5a32bd93cdafd4ae4545a38b
Safa Günay <safagunay96@gmail.com>
2024-11-30 14:07:19 +0300

    refactor(consumer): update folder structure

---
+++ src/consumer/location/consumeLocationQueue.ts
@@ -0,0 +1,64 @@
+import { pid } from 'node:process';
+import { getAreas } from 'src/app/area';
+import { AreaEntity, LocationDto, LogEntity } from 'src/core';
+import { CacheService, LoggerService, QueueService } from 'src/infra';
+import { Repository } from 'typeorm';
+import { LOG_CONTEXT } from '../common/constants';
+import { logLocationAreaOverlaps } from 'src/app/log';
+
+export async function consumeLocationQueue(
+  queueService: QueueService,
+  loggerService: LoggerService,
+  cacheService: CacheService,
+  areaRepository: Repository<AreaEntity>,
+  logRepository: Repository<LogEntity>,
+) {
+  let ackedCount = 0;
+  let nackedCount = 0;
+  let createdLogCount = 0;
+
+  let areas = await getAreas(areaRepository);
+  let areasLastUpdateTime = await cacheService.getAreasLastUpdateTime();
+  let reloadingAreas = false;
+
+  queueService.consumeLocationQueue(
+    `pid:${pid}`,
+    async (message, ack, nack) => {
+      try {
+        const location = JSON.parse(message.content.toString()) as LocationDto;
+        const latestAreasLastUpdateTime =
+          await cacheService.getAreasLastUpdateTime();
+        if (
+          latestAreasLastUpdateTime !== areasLastUpdateTime &&
+          !reloadingAreas
+        ) {
+          reloadingAreas = true;
+          areas = await getAreas(areaRepository);
+          areasLastUpdateTime = latestAreasLastUpdateTime;
+          loggerService.log('Reloaded areas from db', LOG_CONTEXT);
+          reloadingAreas = false;
+        }
+        const res = await logLocationAreaOverlaps(
+          areas,
+          location,
+          logRepository,
+        );
+        createdLogCount += res.length;
+
+        ack(message);
+        ackedCount += 1;
+      } catch (err) {
+        loggerService.error(`Error consuming message: ${err}`, LOG_CONTEXT);
+        nack(message);
+        nackedCount += 1;
+      }
+    },
+  );
+
+  setInterval(() => {
+    loggerService.log(
+      `acked: ${ackedCount}, nacked: ${nackedCount}, logs: ${createdLogCount}`,
+      LOG_CONTEXT,
+    );
+  }, 10000);
+}
--- src/consumer/main.ts
+++ src/consumer/main.ts
@@ -8,69 +8,8 @@ import {
 } from 'src/infra';
 import { ConsumerModule } from './Consumer.module';
 import { Repository } from 'typeorm';
-import { AreaEntity, LocationDto, LogEntity } from 'src/core';
-import { getAreas } from 'src/app/area';
-import { pid } from 'node:process';
-import { logLocationAreaOverlaps } from 'src/app/log/logLocationAreaOverlaps';
-
-const LOG_CONTEXT = 'CONSUMER';
-
-async function startConsume(
-  queueService: QueueService,
-  loggerService: LoggerService,
-  cacheService: CacheService,
-  areaRepository: Repository<AreaEntity>,
-  logRepository: Repository<LogEntity>,
-) {
-  let ackedCount = 0;
-  let nackedCount = 0;
-  let createdLogCount = 0;
-
-  let areas = await getAreas(areaRepository);
-  let areasLastUpdateTime = await cacheService.getAreasLastUpdateTime();
-  let reloadingAreas = false;
-
-  queueService.consumeLocationQueue(
-    `pid:${pid}`,
-    async (message, ack, nack) => {
-      try {
-        const location = JSON.parse(message.content.toString()) as LocationDto;
-        const latestAreasLastUpdateTime =
-          await cacheService.getAreasLastUpdateTime();
-        if (
-          latestAreasLastUpdateTime !== areasLastUpdateTime &&
-          !reloadingAreas
-        ) {
-          reloadingAreas = true;
-          areas = await getAreas(areaRepository);
-          areasLastUpdateTime = latestAreasLastUpdateTime;
-          loggerService.log('Reloaded areas from db', LOG_CONTEXT);
-          reloadingAreas = false;
-        }
-        const res = await logLocationAreaOverlaps(
-          areas,
-          location,
-          logRepository,
-        );
-        createdLogCount += res.length;
-
-        ack(message);
-        ackedCount += 1;
-      } catch (err) {
-        loggerService.error(`Error consuming message: ${err}`, LOG_CONTEXT);
-        nack(message);
-        nackedCount += 1;
-      }
-    },
-  );
-
-  setInterval(() => {
-    loggerService.log(
-      `acked: ${ackedCount}, nacked: ${nackedCount}, logs: ${createdLogCount}`,
-      LOG_CONTEXT,
-    );
-  }, 10000);
-}
+import { AreaEntity, LogEntity } from 'src/core';
+import { consumeLocationQueue } from './location/consumeLocationQueue';
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(ConsumerModule);
@@ -80,7 +19,7 @@ async function bootstrap() {
   const logRepository = app.get(LOG_REPOSITORY) as Repository<LogEntity>;
   const queueService = app.get(QueueService);
 
-  startConsume(
+  consumeLocationQueue(
     queueService,
     loggerService,
     cacheService,
